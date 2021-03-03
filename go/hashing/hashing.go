12769b4fccc0774a241196e317ea3a53135faddf
MathisBurger <57145815+MathisBurger@users.noreply.github.com>
2021-03-03 12:22:33 +0100

    Create hashing.go

---
+++ go/hashing/hashing.go
@@ -0,0 +1,39 @@
+func HashPassword(pwd string) string {
+	salt := generateSalt()
+	hash := hashPassword(pwd, salt)
+	for i := 0; i < 6; i++ {
+		hash = hashPassword(hash, salt)
+	}
+	return hash
+
+}
+
+func generateSalt() []byte {
+	var salt = make([]byte, 16)
+	_, err := rand.Read(salt[:])
+	if err != nil {
+		panic(err.Error())
+	}
+	return salt
+}
+
+func hashPassword(password string, salt []byte) string {
+	var passwordBytes = []byte(password)
+	var sha512Hasher = sha512.New()
+	passwordBytes = append(passwordBytes, salt...)
+	sha512Hasher.Write(passwordBytes)
+	var hashedPasswordBytes = sha512Hasher.Sum(nil)
+	var base64EncodedPasswordHash = hex.EncodeToString(hashedPasswordBytes)
+	var encodedSalt = hex.EncodeToString(salt)
+	return encodedSalt + "$" + base64EncodedPasswordHash
+}
+
+func DoPasswordsMatch(hashedPassword, currPassword string,
+	salt string) bool {
+	slt, _ := hex.DecodeString(salt)
+	currPasswordHash := hashPassword(currPassword, slt)
+	for i := 0; i < 6; i++ {
+		currPasswordHash = hashPassword(currPasswordHash, slt)
+	}
+	return hashedPassword == currPasswordHash
+}
